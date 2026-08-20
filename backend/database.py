ee5caa7c6fdb4be49f33ea1689c3851ffdf47724
darapunenibhavya <darapunenibhavya@gmail.com>
2026-08-20 10:03:48 +0530

    Update MongoDB TLS connection settings

--- backend/database.py
+++ backend/database.py
@@ -2,19 +2,34 @@ from pymongo import MongoClient
 import os
 
 
-# MongoDB Connection
+# =========================================================
+# MONGODB CONNECTION
+# =========================================================
+
 MONGODB_URI = os.getenv(
     "MONGODB_URI",
     "mongodb://localhost:27017/"
 )
 
-client = MongoClient(MONGODB_URI)
+
+client = MongoClient(
+    MONGODB_URI,
+    tls=True,
+    serverSelectionTimeoutMS=30000
+)
 
 
-# Database
+# =========================================================
+# DATABASE
+# =========================================================
+
 db = client["VisionInspectAI"]
 
 
-# Collections
+# =========================================================
+# COLLECTIONS
+# =========================================================
+
 users_collection = db["users"]
+
 history_collection = db["inspection_history"]
